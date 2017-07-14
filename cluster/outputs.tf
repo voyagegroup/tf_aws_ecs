610bb9878dce9d4cf3a63f426de7db682c6c24aa
nishigori <nishigori.tak@gmail.com>
2017-07-14 16:20:41 +0900

    More outputs

--- cluster/outputs.tf
+++ cluster/outputs.tf
@@ -2,6 +2,10 @@ output "cluster_id" {
   value = "${aws_ecs_cluster.main.id}"
 }
 
+output "cloudwatch_log_group_name" {
+  value = "${aws_cloudwatch_log_group.ecs_agent.name}"
+}
+
 output "cloudwatch_log_group_arn" {
   value = "${aws_cloudwatch_log_group.ecs_agent.arn}"
 }
