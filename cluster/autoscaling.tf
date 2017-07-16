c54bdba41eec1057bf2e7b61b886769c2424c3fb
nishigori <nishigori.tak@gmail.com>
2017-07-17 02:57:43 +0900

    autoscaling_policy as simply

--- cluster/autoscaling.tf
+++ cluster/autoscaling.tf
@@ -4,17 +4,27 @@
  * The below resources is optional.
  */
 
-// Memory Reservation
+resource "aws_autoscaling_policy" "scale_out" {
+  count                  = "${ length(keys(var.autoscale_thresholds)) != 0 ? 1 : 0 }"
 
-resource "aws_autoscaling_policy" "scale_out_memory_high" {
-  count                  = "${ lookup(var.autoscale_thresholds, "memory_reservation_high") != "" ? 1 : 0 }"
-
-  name                   = "${aws_ecs_cluster.main.name}-ScaleOut-MemoryReservation-High"
+  name                   = "${aws_ecs_cluster.main.name}-ECSCluster-ScaleOut"
   autoscaling_group_name = "${aws_autoscaling_group.app.name}"
   scaling_adjustment     = "${var.scale_out_adjustment}"
   adjustment_type        = "ChangeInCapacity"
   cooldown               = "${var.autoscale_cooldown}"
 }
+
+resource "aws_autoscaling_policy" "scale_in" {
+  count                  = "${ length(keys(var.autoscale_thresholds)) != 0 ? 1 : 0 }"
+
+  name                   = "${aws_ecs_cluster.main.name}-ECSCluster-ScaleOut"
+  autoscaling_group_name = "${aws_autoscaling_group.app.name}"
+  scaling_adjustment     = "${var.scale_in_adjustment}"
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = "${var.autoscale_cooldown}"
+}
+
+// Memory Reservation
 
 resource "aws_cloudwatch_metric_alarm" "cluster_memory_high" {
   count               = "${ lookup(var.autoscale_thresholds, "memory_reservation_high") != "" ? 1 : 0 }"
@@ -36,19 +46,9 @@ resource "aws_cloudwatch_metric_alarm" "cluster_memory_high" {
 
   ok_actions          = ["${compact(var.scale_out_ok_actions)}"]
   alarm_actions       = [
-    "${aws_autoscaling_policy.scale_out_memory_high.arn}",
+    "${aws_autoscaling_policy.scale_out.arn}",
     "${compact(var.scale_out_more_alarm_actions)}",
   ]
-}
-
-resource "aws_autoscaling_policy" "scale_in_memory_low" {
-  count                  = "${ lookup(var.autoscale_thresholds, "memory_reservation_low") != "" ? 1 : 0 }"
-
-  name                   = "${aws_ecs_cluster.main.name}-ScaleOut-MemoryReservation-Low"
-  autoscaling_group_name = "${aws_autoscaling_group.app.name}"
-  scaling_adjustment     = "${var.scale_in_adjustment}"
-  adjustment_type        = "ChangeInCapacity"
-  cooldown               = "${var.autoscale_cooldown}"
 }
 
 resource "aws_cloudwatch_metric_alarm" "cluster_memory_low" {
@@ -71,22 +71,12 @@ resource "aws_cloudwatch_metric_alarm" "cluster_memory_low" {
 
   ok_actions          = ["${compact(var.scale_in_ok_actions)}"]
   alarm_actions       = [
-    "${aws_autoscaling_policy.scale_in_memory_low.arn}",
+    "${aws_autoscaling_policy.scale_in.arn}",
     "${compact(var.scale_in_more_alarm_actions)}",
   ]
 }
 
 // CPU Reservation
-
-resource "aws_autoscaling_policy" "scale_out_cpu_high" {
-  count                  = "${ lookup(var.autoscale_thresholds, "cpu_reservation_high") != "" ? 1 : 0 }"
-
-  name                   = "${aws_ecs_cluster.main.name}-ScaleOut-CPUReservation-High"
-  autoscaling_group_name = "${aws_autoscaling_group.app.name}"
-  scaling_adjustment     = "${var.scale_out_adjustment}"
-  adjustment_type        = "ChangeInCapacity"
-  cooldown               = "${var.autoscale_cooldown}"
-}
 
 resource "aws_cloudwatch_metric_alarm" "cpu_reservation_high" {
   count               = "${ lookup(var.autoscale_thresholds, "cpu_reservation_high") != "" ? 1 : 0 }"
@@ -108,19 +98,9 @@ resource "aws_cloudwatch_metric_alarm" "cpu_reservation_high" {
 
   ok_actions          = ["${compact(var.scale_out_ok_actions)}"]
   alarm_actions       = [
-    "${aws_autoscaling_policy.scale_out_cpu_high.arn}",
+    "${aws_autoscaling_policy.scale_out.arn}",
     "${compact(var.scale_out_more_alarm_actions)}",
   ]
-}
-
-resource "aws_autoscaling_policy" "scale_in_cpu_low" {
-  count                  = "${ lookup(var.autoscale_thresholds, "cpu_reservation_low") != "" ? 1 : 0 }"
-
-  name                   = "${aws_ecs_cluster.main.name}-ScaleOut-CPUReservation-Low"
-  autoscaling_group_name = "${aws_autoscaling_group.app.name}"
-  scaling_adjustment     = "${var.scale_in_adjustment}"
-  adjustment_type        = "ChangeInCapacity"
-  cooldown               = "${var.autoscale_cooldown}"
 }
 
 resource "aws_cloudwatch_metric_alarm" "cpu_reservation_low" {
@@ -143,7 +123,7 @@ resource "aws_cloudwatch_metric_alarm" "cpu_reservation_low" {
 
   ok_actions          = ["${compact(var.scale_in_ok_actions)}"]
   alarm_actions       = [
-    "${aws_autoscaling_policy.scale_in_cpu_low.arn}",
+    "${aws_autoscaling_policy.scale_in.arn}",
     "${compact(var.scale_in_more_alarm_actions)}",
   ]
 }
