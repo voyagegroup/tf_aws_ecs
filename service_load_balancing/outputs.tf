a401bb1861214bbfc7e3c8e8b78bd09e80a56d06
nishigori <nishigori.tak@gmail.com>
2018-03-27 15:53:02 +0900

    more outputs

--- service_load_balancing/outputs.tf
+++ service_load_balancing/outputs.tf
@@ -1,3 +1,11 @@
 output "service_name" {
   value = "${aws_ecs_service.main.name}"
 }
+
+output "log_group_name" {
+  value = "${aws_cloudwatch_log_group.app.0.name}"
+}
+
+output "log_group_arn" {
+  value = "${aws_cloudwatch_log_group.app.0.arn}"
+}
